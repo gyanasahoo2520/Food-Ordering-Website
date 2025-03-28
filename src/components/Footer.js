841fb352119b56ea0c6616cb02417872220cf830
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2025-03-28 11:22:30 +0530

    Food Junction

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+
+import heartt from "../Images/heartt.png";
+export const Footer = () => {
+
+
+  return (
+    <div className=" flex justify-center align-middle w-auto  text-2xl pt-5 pr-2.5 pb-2 pl-2.5  mb-5 rounded-md shadow-2xl shadow-slate-400">
+      <strong className="mx-1">
+
+        <b>Food Junction</b>
+      </strong>
+      Created By
+
+
+      <a className=" text-red-600 hover:text-purple-800 text-2xl mx-1 "
+        href="https://www.linkedin.com/in/gyana-ranjan-sahoo-a39061225/"
+        target="_blank"
+        title="Gyana's Linkedin Profile"
+      >
+        <b>Gyana Ranjan Sahoo</b></a>
+      
+      
+    </div>
+  );
+
+};
+
+
+
+
+
